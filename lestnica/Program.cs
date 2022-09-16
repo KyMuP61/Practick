4eef85b648cea45a7361d88c80f38a87fb4a4d7a
Alexander Furmanov <kymapcah61@gmail.com>
2022-09-16 18:03:48 +0300

    do void Stairs

--- lestnica/Program.cs
+++ lestnica/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+int a = 4;
+
+void Stairs (int n)
+{
+    List<string> arr = new List<string> (4) {};
+    foreach (var i in arr)
+    {
+        arr.Add(" ");
+    }
+        while ( n>0 )
+    {
+        arr[n-1] = "#";    
+        foreach (var item in arr)
+        {
+            Console.Write(item);
+        }
+        n--;
+        Console.WriteLine(string.Empty);
+    }
+}
+Stairs(a);
+
+
